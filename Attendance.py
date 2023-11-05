d06af1dde40338547075b81819ba4708a760ea5b
imohdalam <mdshabbiralam1@gmail.com>
2023-11-05 19:39:50 +0530

    Added Treading for Voice and Display Message

--- Attendance.py
+++ Attendance.py
@@ -1,9 +1,13 @@
+import logging
+import os
+import tkinter as tk
+from datetime import datetime
+
 import cv2
 import face_recognition
-import os
-from datetime import datetime
 import pyttsx3
-import logging
+import pytz
+import threading
 
 # Initialize the text-to-speech engine
 engine = pyttsx3.init()
@@ -11,48 +15,34 @@ engine = pyttsx3.init()
 # Initialize a logger for error and event logging
 logging.basicConfig(filename='attendance.log', level=logging.INFO)
 
-# Function to mark attendance for known persons
+
+# Function to mark attendance for known persons with date, day, and IST
 def mark_attendance(name):
     try:
         if name != "Unknown":
+            now = datetime.now(pytz.timezone('Asia/Kolkata'))  # Get current time in IST
+            date = now.strftime('%Y-%m-%d')
+            day = now.strftime('%A')
+            time = now.strftime('%H:%M:%S')
             with open('Attendance.csv', 'a') as f:
-                now = datetime.now()
-                dt_string = now.strftime('%H:%M:%S')
-                f.write(f'{name},{dt_string}\n')
+                f.write(f'{name},{date},{day},{time}\n')
                 log_attendance_event(name)  # Log attendance event
-    except FileNotFoundError:
-        log_error("File 'Attendance.csv' not found")  # Log the error
+    except (FileNotFoundError, PermissionError) as e:
+        log_error(f"Error while marking attendance: {str(e)}")  # Log the error
     except Exception as e:
-        log_error(f'Error while marking attendance: {str(e)}')  # Log the error
+        log_error(f'Unexpected error while marking attendance: {str(e)}')  # Log the error
 
-# Function to retrieve records for multiple persons by names (case-insensitive)
-def get_records_by_names(names):
-    records = []
-    try:
-        with open('Attendance.csv', 'r') as f:
-            for line in f:
-                parts = line.strip().split(',')
-                if len(parts) == 2 and parts[0].lower() in [name.lower() for name in names]:
-                    records.append(line.strip())
-    except FileNotFoundError:
-        records.append(f"File 'Attendance.csv' not found")
-    except Exception as e:
-        records.append(f"Error occurred: {str(e)}")
-
-    if not records:
-        for name in names:
-            records.append(f"{name}, No record found")
-    return records
 
 # Function to log attendance events
 def log_attendance_event(name):
     logging.info(f'Attendance marked for {name}')
 
+
 # Function to log errors
 def log_error(message):
     logging.error(message)
 
-# Function to recognize faces in the current frame and play voice messages
+
 def recognize_faces(frame, known_faces, known_names, confidence_threshold=0.6):
     try:
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
@@ -71,23 +61,74 @@ def recognize_faces(frame, known_faces, known_names, confidence_threshold=0.6):
 
                 if confidence_values[match_index] <= confidence_threshold:
                     name = known_names[match_index]
-                    engine.say("Identity confirmed, and Attendance Marked")
+                    message = f"Attendance marked for {name}"
+                    voice_thread = threading.Thread(target=play_voice_message, args=(message,))
+                    voice_thread.start()
+                    show_popup_message(message)
                 else:
-                    engine.say("Not Registered")
+                    message = f"{name} is not registered"
+                    voice_thread = threading.Thread(target=play_voice_message, args=(message,))
+                    voice_thread.start()
+                    show_popup_message(message)
             else:
-                engine.say("Not Registered")
+                message = "Not Registered"
+                voice_thread = threading.Thread(target=play_voice_message, args=(message,))
+                voice_thread.start()
+                show_popup_message(message)
 
             mark_attendance(name)
-
             log_attendance_event(name)  # Log attendance event
     except Exception as e:
         log_error(f'Error while recognizing faces: {str(e)}')  # Log the error
 
+
+# Function to display a popup message using Tkinter
+def show_popup_message(message):
+    popup = tk.Tk()
+    popup.title("Face Recognition")
+
+    # Get the screen width and height
+    screen_width = popup.winfo_screenwidth()
+    screen_height = popup.winfo_screenheight()
+
+    # Calculate the position to center the window
+    x = (screen_width - 300) // 2  # Adjust 300 to the desired width
+    y = (screen_height - 600) // 2  # Adjust 600 to the desired height
+
+    # Set the window size and position
+    popup.geometry(f"300x600+{x}+{y}")
+
+    label = tk.Label(popup, text=message, font=("Arial", 24))  # Adjust the font and size as needed
+    label.pack(fill="both", expand=True, padx=20, pady=20)  # Center the label
+
+    popup.after(4000, popup.destroy)  # Close the popup after 4 seconds
+
+    popup.mainloop()
+
+
+
+# Function to play a voice message
+def play_voice_message(message):
+    engine.say(message)
     engine.runAndWait()
 
+
 def main():
+    cap = None
     try:
-        # Load known faces and other setup (not shown in this snippet)
+        # Load known faces and names from the "known_faces" directory
+        known_faces = []
+        known_names = []
+        known_faces_dir = 'known_faces'
+
+        for filename in os.listdir(known_faces_dir):
+            if filename.endswith(('.jpg', '.jpeg', '.png')):
+                # Load the image using face_recognition
+                image = face_recognition.load_image_file(os.path.join(known_faces_dir, filename))
+                encoding = face_recognition.face_encodings(image)
+                if encoding:
+                    known_faces.append(encoding[0])  # Assuming there's only one face per image
+                    known_names.append(os.path.splitext(filename)[0])  # Use the filename as the name
 
         cap = cv2.VideoCapture(0)  # Initialize camera capture
 
@@ -103,15 +144,17 @@ def main():
     except KeyboardInterrupt:
         # Handle a user interrupt (e.g., Ctrl+C) gracefully
         print("User interrupted the application.")
+    except (FileNotFoundError, PermissionError) as e:
+        log_error(f"Error: {str(e)}")
     except Exception as e:
         # Handle unexpected errors and log them
-        print(f"An unexpected error occurred: {str(e)}")
+        log_error(f"An unexpected error occurred: {str(e)}")
 
     finally:
-        # Clean up resources even if an error occurred
-        cap.release()  # Release the camera
+        if cap is not None:
+            cap.release()  # Release the camera
         cv2.destroyAllWindows()  # Close any open windows
+
 
 if __name__ == "__main__":
     main()
-
